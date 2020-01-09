ec3c3bb56a1585726ebd915a52d961810361ded3
Keren Shina <kerenshina1996@gmail.com>
2020-01-09 22:04:20 +0200

    splitted the ExpressionTypes to binary and unary operators, value, expression interpreter

---
+++ simulator/BinaryOperator.h
@@ -0,0 +1,47 @@
+#ifndef EXPRESSIONS_BINARY_OPERATOR_H
+#define EXPRESSIONS_BINARY_OPERATOR_H
+
+#include "Expression.h"
+
+using namespace std;
+
+class BinaryOperator: public Expression {
+
+    protected:
+        Expression *right, *left;
+
+    public:
+        BinaryOperator(Expression *right, Expression *left);
+        virtual double calculate() = 0;
+};
+
+class Plus: public BinaryOperator {
+    public:
+        Plus(Expression* exp1, Expression* exp2);
+        double calculate();
+        ~Plus();
+};
+
+class Minus: public BinaryOperator {
+    public:
+        Minus(Expression* exp1, Expression* exp2);
+        double calculate();
+        ~Minus();
+};
+
+class Mul: public BinaryOperator {
+    public:
+        Mul(Expression* exp1, Expression* exp2);
+        double calculate();
+        ~Mul();
+};
+
+class Div: public BinaryOperator {
+    public:
+        Div(Expression* exp1, Expression* exp2);
+        double calculate();
+        ~Div();
+};
+
+
+#endif //EXPRESSIONS_BINARY_OPERATOR_H
