5b52ad983e3eb721ec3f77805be4773af4457bad
sdonte980 <59441583+sdonte980@users.noreply.github.com>
2020-01-19 17:19:55 +0200

    updated

--- simulator/ConditionCommandTypes.h
+++ simulator/ConditionCommandTypes.h
@@ -16,19 +16,14 @@ class IfCommand : public ConditionParser {
     map<string, Command*> commands;
 public:
     IfCommand() {}
-    void getCommands(map<string, Command*> commands);
     int execute(int position);
     ~IfCommand();
 
-private:
-    bool createCondition(string var1, string operatorStr,string var2);
 };
 
 class LoopCommand : public ConditionParser {
-   // map<string, Command*> commands;
 public:
     LoopCommand() {}
-    void getCommands(map<string, Command*> commands);
     int execute(int position);
     ~LoopCommand();
 
