645ba82be137ebfb09a515a96ef096b108eadec4
sdonte980 <59441583+sdonte980@users.noreply.github.com>
2020-01-19 17:22:27 +0200

    updated

--- simulator/Interpreter.cpp
+++ simulator/Interpreter.cpp
@@ -17,20 +17,11 @@ using namespace std;
 Interpreter::Interpreter(string fileName) {
     this->fileName = fileName;
     tokens= lexer();
-    mapCommands(0);
-    cout<<"##### Print tokens: #####" << endl;
-    for (int i = 0; i < tokens.size(); i++) {
-        cout << tokens[i] << endl;
-    }
-    cout<<"##### Print commands: #####"<<endl;
-    for (auto const& pair:commands){
-        cout<<pair.first<<" "<<pair.second<<endl;
-    }
-    cout<<"##### going to Parser: ##### "<<endl;
+    mapCommands();
     parser();
-
 }
 
+//This function gets a char and returns if it's in the operator vector.
 bool Interpreter::inVec(char c) {
     for (char i : this->operatorVec) {
         if (c == i) {
@@ -40,144 +31,129 @@ bool Interpreter::inVec(char c) {
     return false;
 }
 
+//This function reads from "file_name" file and split it to vector of string tokens.
 vector<string> Interpreter::lexer() {
-
     vector<string> tokens;
     vector<string> lines = this->getLinesFromFile();
     for (int k = 0; k < lines.size(); k++) {
-            string newWord;
-            int i, j,size = strlen(lines[k].c_str());
-            for (i = 0; i < size; i++) {
-                if (lines[k][i] == '\t') {
-                    continue;
+        string newWord;
+        int i, j,size = strlen(lines[k].c_str());
+        for (i = 0; i < size; i++) {
+            if (lines[k][i] == '\t') {
+                continue;
+            }
+
+            if (lines[k][i] == '"') {
+                newWord += lines[k][i];
+                j = i + 1;
+                while (lines[k][j] != '"') {
+                    newWord += lines[k][j];
+                    j++;
+                }
+                newWord += lines[k][j];
+                i = j;
+            } else if (lines[k][i] == '-' && lines[k][i + 1] == '>') {
+
+                if (newWord.length() > 0) {
+                    tokens.push_back(newWord);
+                    newWord = "";
                 }
 
-                if (lines[k][i] == '"') {
+                newWord += lines[k][i];
+                newWord += lines[k][i + 1];
+                tokens.push_back(newWord);
+                newWord = "";
+                i += 1;
+            } else if (inVec(lines[k][i]) == 1) {
+
+                if (newWord.length() > 0) {
+                    tokens.push_back(newWord);
+                    newWord = "";
+                }
+                if (inVec(lines[k][i + 1]) == 1) {
                     newWord += lines[k][i];
-                    j = i + 1;
-                    while (lines[k][j] != '"') {
-                        newWord += lines[k][j];
-                        j++;
-                    }
-                    newWord += lines[k][j];
-                    i = j;
-                } else if (lines[k][i] == '-' && lines[k][i + 1] == '>') {
-
-                    if (newWord.length() > 0) {
-                        tokens.push_back(newWord);
-                        newWord = "";
-                    }
-
+                    newWord += lines[k][i + 1];
+                    tokens.push_back(newWord);
+                    newWord = "";
+                    j = i + 2;
+                    // creates the <-
+                } else if (lines[k][i + 1] == '-') {
                     newWord += lines[k][i];
                     newWord += lines[k][i + 1];
                     tokens.push_back(newWord);
                     newWord = "";
                     i += 1;
-                } else if (inVec(lines[k][i]) == 1) {
+                    continue;
+                } else {
 
-                    if (newWord.length() > 0) {
-                        tokens.push_back(newWord);
-                        newWord = "";
-                    }
-                    if (inVec(lines[k][i + 1]) == 1) {
-                        newWord += lines[k][i];
-                        newWord += lines[k][i + 1];
-                        tokens.push_back(newWord);
-                        newWord = "";
-                        j = i + 2;
-                        // creates the <-
-                    } else if (lines[k][i + 1] == '-') {
-                        newWord += lines[k][i];
-                        newWord += lines[k][i + 1];
-                        tokens.push_back(newWord);
-                        newWord = "";
-                        i += 1;
-                        continue;
-                    } else {
-
-                        newWord += lines[k][i];
-                        tokens.push_back(newWord);
-                        newWord = "";
-                        j = i + 1;
-                    }
-
-                    for (j; lines[k][j] != '{' && j < size; j++) {
-                        if (lines[k][j] != ' ') {
-                            newWord += lines[k][j];
-                        }
-                    }
+                    newWord += lines[k][i];
                     tokens.push_back(newWord);
                     newWord = "";
+                    j = i + 1;
+                }
 
-                    if (lines[k][j] == '{') {
+                for (j; lines[k][j] != '{' && j < size; j++) {
+                    if (lines[k][j] != ' ') {
                         newWord += lines[k][j];
-                        tokens.push_back(newWord);
-                        newWord = "";
-                        j++;
-                    }
-                    i = j;
-                } else if ((lines[k][i] != '(') && (lines[k][i] != ')')
-                && (lines[k][i] != ',') && (lines[k][i] != ' ')) {
-                    newWord += lines[k][i];
-                } else {
-                    if(newWord.length() > 0) {
-                        tokens.push_back(newWord);
-                        newWord = "";
                     }
                 }
-            }
-            if (newWord.empty() == 0) {
                 tokens.push_back(newWord);
+                newWord = "";
+
+                if (lines[k][j] == '{') {
+                    newWord += lines[k][j];
+                    tokens.push_back(newWord);
+                    newWord = "";
+                    j++;
+                }
+                i = j;
+            } else if ((lines[k][i] != '(') && (lines[k][i] != ')')
+                       && (lines[k][i] != ',') && (lines[k][i] != ' ')) {
+                newWord += lines[k][i];
+            } else {
+                if(newWord.length() > 0) {
+                    tokens.push_back(newWord);
+                    newWord = "";
+                }
             }
         }
-
+        if (newWord.empty() == 0) {
+            tokens.push_back(newWord);
+        }
+    }
     return tokens;
 }
+
+//This function gets a string and returns true if it's in the symbolTable map.
 bool Interpreter::isInSymbolTable(string symbol){
     return (symbolTable.find(symbol) != symbolTable.end()) ;
 }
+
+//This function parse the tokens and execute the commands by order.
 void Interpreter::parser() {
     int index = 0;
-cout<<"Tokens size: "<<tokens.size()<<endl;
     while (index < tokens.size()) {
         string currentToken = tokens[index];
         if (isInSymbolTable(currentToken)) {
             currentToken = tokens[index+1];
             index++;
         }
-        cout<< "index: "<<index<<" Key: " <<currentToken;
         map<string, Command *>::iterator itr = commands.find(currentToken);
         if (itr != commands.end()) {
             Command *c = itr->second;
             vector<string> parameters = getParameters(index);
-            //if (currentToken.compare("=")==0){
-            //    parameters.push_back(tokens[index-1]);
-           // }
-            cout<<" Parametrs: [";
-            printVector(parameters);
-            cout<<"]"<<endl;
             int skip =  c->execute(index);
             index += skip;
         }
         index++;
     }
-    cout<<"##### TO SIMULATOR VARS #####"<<endl;
-    for(map<string, Variable>::const_iterator it = inputVals.begin();
-        it != inputVals.end(); ++it)
-    {
-        std::cout << it->first <<endl;
-    }cout<<"##### FROM SIMULATOR VARS  #####"<<endl;
-    for(map<string, Variable>::const_iterator it = outputVals.begin();
-        it != outputVals.end(); ++it)
-    {
-        std::cout << it->first <<endl;
-    }
+}
 
+Interpreter::~Interpreter() {
 
 }
 
-Interpreter::~Interpreter() {}
-
+//This function reads the file and turn it to vector of strings
 vector<string> Interpreter::getLinesFromFile() {
     vector<string> lines;
     ifstream file;
@@ -197,7 +173,8 @@ vector<string> Interpreter::getLinesFromFile() {
     return lines;
 }
 
-
+//This function get's position of token and returns vector of strings
+//as the parameters for the execution.
 vector<string> Interpreter::getParameters(int position) {
     map<string, Command *>::iterator itr;
     map<string, Command *> commandsToScope;
@@ -263,7 +240,8 @@ vector<string> Interpreter::getParameters(int position) {
     return parameters;
 }
 
-void Interpreter::mapCommands(int index) {
+//This function init the map of commands.
+void Interpreter::mapCommands() {
     commands["openDataServer"]= new OpenServerCommand;
     commands["connectControlClient"]= new ConnectCommand;
     commands["var"]= new DefineVarCommand(&symbolTable);
@@ -274,12 +252,3 @@ void Interpreter::mapCommands(int index) {
     commands["if"]= new IfCommand;
     commands["takeoff"]= new FuncCommand;
 }
-
-
-void Interpreter::printVector(vector<string> vector) {
-for(int i=0 ;i < vector.size(); i++){
-    cout<<vector[i]<<", ";
-    }
-}
-
-
